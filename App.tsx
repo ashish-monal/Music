aaf5b26d48708b42007f1ee7184b7032cae229b6
Ashish Ranjan <ashish.ranjan@cardinaltsinc.net>
2023-05-04 07:10:19 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
+import React from 'react';
+import MusicPlayer from './screens/MusicPlayer';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <MusicPlayer />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ model/Data.js
@@ -0,0 +1,39 @@
+const songs = [
+  {
+    id: 1,
+    title: '19th Floor',
+    artist: 'Bobby Richards',
+    artwork: require('../assets/img/img1.jpg'),
+    url: require('../assets/audio/One.mp3'),
+  },
+  {
+    id: 2,
+    title: 'Awful',
+    artist: 'josh Pan',
+    artwork: require('../assets/img/img2.jpg'),
+    url: require('../assets/audio/Two.mp3'),
+  },
+  {
+    id: 3,
+    title: 'Something is going on',
+    artist: 'Godmode',
+    artwork: require('../assets/img/img3.jpg'),
+    url: require('../assets/audio/Three.mp3'),
+  },
+  {
+    id: 4,
+    title: 'Book the rental with it',
+    artist: 'Rage',
+    artwork: require('../assets/img/img4.jpg'),
+    url: require('../assets/audio/Four.mp3'),
+  },
+  {
+    id: 5,
+    title: 'Crimson Fly',
+    artist: 'Huma-Huma',
+    artwork: require('../assets/img/img5.jpg'),
+    url: require('../assets/audio/Five.mp3'),
+  },
+];
+
+export default songs;
